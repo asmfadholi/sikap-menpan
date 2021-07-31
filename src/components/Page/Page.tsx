74049d8f251df6902f6f8d698f3aabe1ca974407
Mochammad Fadholi <market.fadholi@gmail.com>
2021-08-01 02:27:49 +0700

    fix: revise config

--- src/components/Page/Page.tsx
+++ src/components/Page/Page.tsx
@@ -1,22 +1,11 @@
 import React from "react";
 import Head from "next/head";
 
-export const appleTouchIcon =
-	"https://nextify.s3-eu-west-1.amazonaws.com/img/meta/180.png";
-export const favIcon32 =
-	"https://nextify.s3-eu-west-1.amazonaws.com/img/meta/32.png";
-export const favIcon16 =
-	"https://nextify.s3-eu-west-1.amazonaws.com/img/meta/16.png";
-export const favIcon =
-	"https://nextify.s3-eu-west-1.amazonaws.com/img/meta/favicon.ico";
-export const safariPinnedTab =
-	"https://nextify.s3-eu-west-1.amazonaws.com/img/meta/512.svg";
-export const preview =
-	"https://nextify.s3-eu-west-1.amazonaws.com/img/meta/preview.jpg";
+const preview = "/icons/icon-512x512.png";
 
 const Page = ({
-	title = "Nextify",
-	description = "React apps using Next.js and Emotion.",
+	title = "Sikap Menpan",
+	description = "Sikap Menpan Apps",
 	image = preview,
 	children,
 }) => (
@@ -25,28 +14,32 @@ const Page = ({
 			<meta httpEquiv="" content="IE=edge" />
 			<meta
 				name="viewport"
-				content="width=device-width, initial-scale=1, maximum-scale=1"
+				content="width=device-width, initial-scale=1, maximum-scale=5"
 			/>
 			<meta name="description" content={description} />
-			<link rel="manifest" href="/site.webmanifest" />
-			<meta name="theme-color" content="#0370F3" />
-			<meta property="og:title" content={title + " - Nextify"} />
+			<link rel="manifest" href="/manifest.json" />
+			<meta name="theme-color" content="#001529" />
+			<meta property="og:title" content={title} />
 			<meta property="og:description" content={description} />
 			<meta property="og:image" content={image} />
 			<meta name="twitter:card" content="summary_large_image" />
 			<meta name="twitter:description" content={description} />
-			<meta name="twitter:title" content={title + " - Nextify"} />
+			<meta name="twitter:title" content={title} />
 			<meta name="twitter:image" content={image} />
 			<link
-				rel="apple-touch-icon"
-				sizes="180x180"
-				href={appleTouchIcon}
+				rel="icon"
+				type="image/png"
+				sizes="32x32"
+				href="/favicon-32x32.png"
 			/>
-			<link rel="mask-icon" href={safariPinnedTab} color="#0370F3" />
-			<link rel="icon" type="image/png" sizes="32x32" href={favIcon32} />
-			<link rel="icon" type="image/png" sizes="16x16" href={favIcon16} />
-			<link rel="shortcut icon" href={favIcon} />
-			<title>{title + " - Nextify"}</title>
+			<link
+				rel="icon"
+				type="image/png"
+				sizes="16x16"
+				href="/favicon-16x16.png"
+			/>
+			<link rel="shortcut icon" href="/favicon-16x16.png" />
+			<title>{`${title} | ${description}`}</title>
 		</Head>
 		{children}
 	</React.Fragment>
